6c4db51bc3a24888cb1cbd8d6edbeeb5db1a5b7a
caizhijian <931617277@qq.com>
2020-04-25 17:49:00 +0800

    admin监控练习

--- spring-cloud-web/src/main/java/com/czj/SpringCloudWebApp.java
+++ spring-cloud-web/src/main/java/com/czj/SpringCloudWebApp.java
@@ -13,6 +13,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class SpringCloudWebApp {
     public static void main(String[] args){
+
         SpringApplication.run(SpringCloudWebApp.class,args);
     }
 }
