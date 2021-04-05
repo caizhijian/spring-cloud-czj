6283cf8007c23e8ec7bbcb79e5eaf648d7aba3ed
caizhijian <931617277@qq.com>
2021-04-05 17:12:38 +0800

    集成kafka

--- spring-cloud-web/src/main/java/com/czj/modules/hello/HelloController.java
+++ spring-cloud-web/src/main/java/com/czj/modules/hello/HelloController.java
@@ -1,8 +1,11 @@
 package com.czj.modules.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.kafka.core.KafkaTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -15,10 +18,21 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping(value = "/hello")
 public class HelloController {
 
+    @Autowired
+    private KafkaTemplate<Object, Object> template;
+
     @GetMapping("/sayHello")
     public String hello(HttpServletRequest request){
         //测试请求头
-        System.out.println(request.getHeader("X-Request-Foo"));;
+        System.out.println(request.getHeader("X-Request-Foo"));
         return "hello";
     }
+
+    @GetMapping("/testKafka/{input}")
+    public String sendFoo(@PathVariable String input) {
+        this.template.send("test", input);
+        return "testKafka";
+    }
+
+
 }
